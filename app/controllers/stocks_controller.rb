6b8706285095042c6b81dc50afb30f9779103def
bran31 <bran31@gmail.com>
2019-02-07 15:47:18 -0500

    added stock lookup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,5 +1,18 @@
 class StocksController < ApplicationController
 
-    
+    def search
+        if params[:stock].present?
+        @stock = Stock.new_from_lookup(params[:stock])
+            if @stock
+                render "users/my_portfolio"
+            else
+                flash[:danger] = "You have entered and invalid stock symbol"
+                redirect_to my_portfolio_path
+            end
+        else
+            flash[:danger] = "You have entered an empty search string"
+            render "users/my_portfolio"
+        end
+    end
 
 end
